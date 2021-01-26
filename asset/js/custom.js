b01359e506b91cff002c4bdfe2ef14a4972044bd
devshisir <contact.shisirimran@gmail.com>
2021-01-26 18:08:04 +0600

    Artist table add

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
     var school = $(window);
     var page = $('html, body');
+
+
+
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 100) {
+            $('#header_top').addClass('animate__animated animate__slideInDown fix');
+        }
+        else {
+            $('#header_top').removeClass('animate__animated animate__slideInDown fix');
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 });
